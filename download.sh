15a31a39aecaa4504d93c52a8f06d4dd44adb1ec
Cliff Lee <clifton.lee@uftwf.org>
2021-11-08 21:45:49 -0500

    Adding ham and spam

---
+++ download.sh
@@ -0,0 +1,24 @@
+
+wget https://spamassassin.apache.org/old/publiccorpus/20021010_easy_ham.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20021010_hard_ham.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20021010_spam.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20030228_easy_ham.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20030228_easy_ham_2.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20030228_hard_ham.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20030228_spam.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20030228_spam_2.tar.bz2
+wget https://spamassassin.apache.org/old/publiccorpus/20050311_spam_2.tar.bz2
+
+
+bunzip2 *.bz2
+
+tar -xf 20021010_easy_ham.tar
+tar -xf 20021010_hard_ham.tar
+tar -xf 20021010_spam.tar
+tar -xf 20030228_easy_ham.tar
+tar -xf 20030228_easy_ham_2.tar
+tar -xf 20030228_hard_ham.tar
+tar -xf 20030228_spam.tar
+tar -xf 20030228_spam_2.tar
+tar -xf 20050311_spam_2.tar
+
